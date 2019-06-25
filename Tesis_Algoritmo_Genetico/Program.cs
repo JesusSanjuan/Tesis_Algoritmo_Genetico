1b53f1dbbc0d47f5080e13681c52c91f5cbca8f7
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-06-25 14:39:07 -0500

    Agregar archivos de proyecto.

---
+++ Tesis_Algoritmo_Genetico/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Tesis_Algoritmo_Genetico
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r= new Random();
+            float numero;
+            do
+            {
+                numero = (float)(r.Next(0, 101)) / 100;
+                Console.WriteLine("Numero float aleatorio: "+numero);
+                
+                //Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+
+            } while (true);
+        }
+    }
+}
