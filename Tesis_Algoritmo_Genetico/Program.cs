403f127141db0d0c7066835137c0c7fb572e14bd
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-06-26 11:57:49 -0500

    Conversion de n numeros decimales a binario

--- Tesis_Algoritmo_Genetico/Program.cs
+++ Tesis_Algoritmo_Genetico/Program.cs
@@ -12,22 +12,101 @@ namespace Tesis_Algoritmo_Genetico
     class Program
     {
         static void Main(string[] args)
-        {
-            
-
-
-           // do
-           // {
+        {    
+            do
+            {
                 Random rand = new Random();
                 // Generate and display 5 random byte (integer) values.
                 byte[] bytes = new byte[5];
                 rand.NextBytes(bytes);
-                Console.WriteLine("Five Doubles between 0 and 5.");
-                for (int ctr = 0; ctr <= 4; ctr++)
-                    Console.Write("{0,8:N3}", rand.NextDouble() * 1000);
+                Double numeroaleatorio = rand.NextDouble() * 200;
+                string str = numeroaleatorio.ToString();
+                Console.Write("Numero alearotio en decimal {0,8:N3} \n", numeroaleatorio);
+                List<string> resultados = new List<string>();
+                resultados=dividircadena(str,5);
+                String primerconversion = convertirbinario(resultados[0]);
+                String segundaconversion = convertirbinariodecimal(resultados[1]);
+                String numerocompletobinario = primerconversion + "." + segundaconversion;
+                Console.Write("Numero decimal en binario: {0:G} \n", numerocompletobinario);
                 Console.ReadKey();
+            } while (true);
+        }
 
-           // } while (true);
+        static string convertirbinario(String cadena)
+        {
+            int Num = Convert.ToInt32(cadena);
+            String cad = "";
+            if (Num > 0)
+            {               
+                while (Num > 0)
+                {
+                    if (Num % 2 == 0)
+                    {
+                        cad = "0" + cad;
+                    }
+                    else
+                    {
+                        cad = "1" + cad;
+                    }
+                    Num = (int)(Num / 2);
+                }
+            }
+            return cad;
+        }
+
+        static List<string> dividircadena(string cadena, int tamano)
+        {
+            String cad1="";
+            String cad2="";
+            int pos = 0;
+            List<string> cadenas = new List<string>();
+            for (int i = 0; i < cadena.Length - 1; i++)
+            {
+                if (cadena[i] =='.')
+                {
+                    pos =i;
+                    break;
+                }
+            }
+            cad1 = cadena.Substring(0, pos);
+            cad2 = cadena.Substring(pos,tamano);
+            cadenas.Add(cad1);
+            cadenas.Add(cad2);
+            return cadenas;
+        }
+
+        static string convertirbinariodecimal(string valor)
+        {
+            double valfloat = Convert.ToDouble(valor);
+            valfloat = valfloat * 2;
+            List<string> binariofraccion = new List<string>();
+            double salida = 0;
+            int contador = 0;
+            do
+            {
+                List<string> resultados = new List<string>();
+                int count = BitConverter.GetBytes(decimal.GetBits((decimal)valfloat)[3])[2];
+                resultados =dividircadena(Convert.ToString(valfloat), count+1);
+                binariofraccion.Add(resultados[0]);
+                salida = Convert.ToDouble(resultados[1]);
+                valfloat = Convert.ToDouble(resultados[1]) * 2;
+                contador++;
+                if(contador==5)
+                {
+                    break;
+                }
+            } while (salida != 0);
+            return convertirstring(binariofraccion);
+        }
+
+        static string convertirstring(List<string> valor)
+        {
+            String palabra="";
+            foreach (string contenido in valor)
+            {
+                palabra = palabra + contenido;
+            }
+            return palabra;
         }
     }
 }
