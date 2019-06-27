86c3646b6a251c9fe407d5a98248812d37ba5ae2
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-06-27 11:40:26 -0500

    Otra manera

--- Tesis_Algoritmo_Genetico/Program.cs
+++ Tesis_Algoritmo_Genetico/Program.cs
@@ -8,107 +8,73 @@ using System.Threading.Tasks;
 
 namespace Tesis_Algoritmo_Genetico
 {
-    
+
     class Program
     {
         static void Main(string[] args)
-        {    
+        {
             do
             {
                 Random rand = new Random();
-                byte[] bytes = new byte[5];
-                rand.NextBytes(bytes);
-                Double numeroaleatorio = rand.NextDouble() * 100;
-                numeroaleatorio = Math.Round(numeroaleatorio, 2);
-                int count = BitConverter.GetBytes(decimal.GetBits((decimal)numeroaleatorio)[3])[2];
-                string str = numeroaleatorio.ToString();
-                Console.Write("Numero alearotio en decimal {0:G} \n", numeroaleatorio);
-                List<string> resultados = new List<string>();
-                
-                resultados =dividircadena(str,count+1);
-                String primerconversion = convertirbinario(resultados[0]);
-                String segundaconversion = convertirbinariodecimal(resultados[1]);
-                String numerocompletobinario = primerconversion + "." + segundaconversion;
-                Console.Write("Numero decimal en binario: {0:G} \n", numerocompletobinario);
+                String entero = "";
+                String flotante = "";
+                for (int x = 0; x < 18; x++)
+                {
+                    int value = rand.Next(0, 2);
+                    if (x < 9)
+                    {
+                        entero = entero + value;
+                    }
+                    else
+                    {
+                        flotante = flotante + value;
+                    }
+                }
+                Console.Write("Numero completo en binario: {0}.{1} \n", entero, flotante);
+                int enterooo = BinarioADecimal(entero);
+                float partedecimal = BinarioADecimalFlotante(flotante);
+                Console.Write("Numero en decimal {0}", concatenacion(enterooo.ToString(), partedecimal.ToString()));
+                Console.Write("\n\n");
                 Console.ReadKey();
             } while (true);
         }
 
-        static string convertirbinario(String cadena)
+        static int BinarioADecimal(String input)
         {
-            int Num = Convert.ToInt32(cadena);
-            String cad = "";
-            if (Num > 0)
-            {               
-                while (Num > 0)
+            char[] array = input.ToCharArray();
+            Array.Reverse(array);
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == '1')
                 {
-                    if (Num % 2 == 0)
-                    {
-                        cad = "0" + cad;
-                    }
-                    else
-                    {
-                        cad = "1" + cad;
-                    }
-                    Num = (int)(Num / 2);
+                    sum += (int)Math.Pow(2, i);
                 }
             }
-            return cad;
+            return sum;
         }
 
-        static List<string> dividircadena(string cadena, int tamano)
+        static float BinarioADecimalFlotante(String input)
         {
-            String cad1="";
-            String cad2="";
-            int pos = 0;
-            List<string> cadenas = new List<string>();
-            for (int i = 0; i < cadena.Length - 1; i++)
+            float[] arrayBase = new float[] { 0.5F, 0.25F, 0.125F, 0.0625F, 0.03125F, 0.015625F, 0.0078125F, 0.00390625F, 0.001953125F, 0.0009765625F };
+            char[] array = input.ToCharArray();
+            //Array.Reverse(array);
+            float sum = 0;
+            for (int i = 0; i < array.Length; i++)
             {
-                if (cadena[i] =='.')
+                if (array[i] == '1')
                 {
-                    pos =i;
-                    break;
+                    sum = sum + arrayBase[i];
                 }
             }
-            cad1 = cadena.Substring(0, pos);
-            cad2 = cadena.Substring(pos,tamano);
-            cadenas.Add(cad1);
-            cadenas.Add(cad2);
-            return cadenas;
+            return sum;
         }
 
-        static string convertirbinariodecimal(string valor)
+        static float concatenacion(String input, String input2)
         {
-            double valfloat = Convert.ToDouble(valor);
-            valfloat = valfloat * 2;
-            List<string> binariofraccion = new List<string>();
-            double salida = 0;
-            int contador = 0;
-            do
-            {
-                List<string> resultados = new List<string>();
-                int count = BitConverter.GetBytes(decimal.GetBits((decimal)valfloat)[3])[2];
-                resultados =dividircadena(Convert.ToString(valfloat), count+1);
-                binariofraccion.Add(resultados[0]);
-                salida = Convert.ToDouble(resultados[1]);
-                valfloat = Convert.ToDouble(resultados[1]) * 2;
-                contador++;
-                if(contador==8)
-                {
-                    break;
-                }
-            } while (salida != 0);
-            return convertirstring(binariofraccion);
-        }
-
-        static string convertirstring(List<string> valor)
-        {
-            String palabra="";
-            foreach (string contenido in valor)
-            {
-                palabra = palabra + contenido;
-            }
-            return palabra;
+            string parcial = input2.Substring(1, 8);
+            String resultado = input + parcial;
+            return Convert.ToSingle(resultado); ;
         }
     }
 }
