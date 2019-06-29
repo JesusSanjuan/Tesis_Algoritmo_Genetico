37fd512a5b8d1c29113617949f5a81f38b828f28
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-06-29 16:25:52 -0500

    Suma terminada

--- Tesis_Algoritmo_Genetico/Program.cs
+++ Tesis_Algoritmo_Genetico/Program.cs
@@ -16,14 +16,17 @@ namespace Tesis_Algoritmo_Genetico
             String num1, num2;
             Console.WriteLine("Primer numero: ");
             num1 = Console.ReadLine();
-            Console.WriteLine("Seundo numero: ");
+            Console.WriteLine("Segundo numero: ");
             num2 = Console.ReadLine();
-            CalcularSuma(num1, num2);
+            Console.WriteLine("Resultado: {0}", CalcularSuma(num1, num2, 2));
+            //decimal x = 7922816251426433759354395033m;
             Console.ReadKey();
         }
 
-        public static string CalcularSuma(string num1, string num2)
+        public static string CalcularSuma(string num1, string num2,int posicion)
         {
+            num1 = num1.Replace(".", "");
+            num2 = num2.Replace(".", "");
             int a1 = num1.Length;
             int a2 = num2.Length;
             char[] array1 = num1.ToCharArray();
@@ -32,18 +35,21 @@ namespace Tesis_Algoritmo_Genetico
             char[] arra2;
             char[] acarreo;
             char[] resultado;
+            char[] resultadoEnviar;
             if (a1>a2)
             {
                 arra1 = new char[num1.Length];
                 arra2 = new char[num1.Length];
                 acarreo = new char[num1.Length];
                 resultado = new char[num1.Length+1];
+                resultadoEnviar = new char[num1.Length + 2];
                 for (int i = 0; i < arra1.Length; i++)
                 {
                     arra1[i] = '0';
                     arra2[i] = '0';
                     acarreo[i] = '0';
                     resultado[i] = '0';
+                    resultadoEnviar[i] = '0';
                 }
 
                 arra1 = num1.ToCharArray();
@@ -59,12 +65,14 @@ namespace Tesis_Algoritmo_Genetico
                 arra2 = new char[num2.Length];
                 acarreo = new char[num2.Length];
                 resultado = new char[num2.Length + 1];
+                resultadoEnviar = new char[num2.Length + 2];
                 for (int i = 0; i < arra1.Length; i++)
                 {
                     arra1[i] = '0';
                     arra2[i] = '0';
                     acarreo[i] = '0';
                     resultado[i] = '0';
+                    resultadoEnviar[i] = '0';
                 }
                 
                 int reducir = arra1.Length - 1;
@@ -75,16 +83,61 @@ namespace Tesis_Algoritmo_Genetico
                 arra2 = num2.ToCharArray();
             }
 
-            for(int x=arra1.Length-1;0<=x;x--)
+            for(int x=arra1.Length;1<=x;x--)
             {
-                int ar = int.Parse(acarreo[x].ToString());
-                int v1 = int.Parse(arra1[x].ToString());
-                int v2 = int.Parse(arra2[x].ToString());
-
+                int ar = int.Parse(acarreo[x - 1].ToString());
+                int v1 = int.Parse(arra1[x - 1].ToString());
+                int v2 = int.Parse(arra2[x - 1].ToString());
                 int resultadocolumna = ar + v1 + v2;
-               
+                string myString = resultadocolumna.ToString();
+                char[] myStringchar = myString.ToCharArray();
+                if (x == 1)
+                {
+                    if (myString.Length == 2)
+                    {
+                        resultado[x - 1] = myStringchar[0];
+                        resultado[x] = myStringchar[1];
+                    }
+                    else
+                    {
+                        resultado[x] = myStringchar[0];
+                    }                        
+                }
+                else
+                {                       
+                    if (myString.Length == 2)
+                    {
+                        acarreo[x - 2] = myStringchar[0];
+                        resultado[x] = myStringchar[1];
+                    }
+                    else
+                    {
+                        resultado[x] = myStringchar[0];
+                    }
+                }
             }
-            return "";
+            int contador = 0;
+            int incremento = 0;
+            for (int x = resultadoEnviar.Length; 0 < x; x--)
+            {                  
+                if (2== contador)
+                {
+                    resultadoEnviar[x - 1] = '.';
+                    incremento = 1;
+                }
+                else
+                {
+                    resultadoEnviar[x-1] = resultado[x - 2+incremento];
+                }
+                contador = contador + 1;
+            }        
+                
+            string resultadoT = new string(resultadoEnviar);
+            if (resultadoEnviar[0] == '0')
+            {
+                resultadoT = resultadoT.Substring(1, resultadoT.Length-1);
+            }
+            return resultadoT;
         }
     }
 }
