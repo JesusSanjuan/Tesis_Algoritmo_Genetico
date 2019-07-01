c5ddcfa82bdf63ef414b8b77f1672cddb6395448
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-07-01 10:22:07 -0500

    Correcion

--- Tesis_Algoritmo_Genetico/Program.cs
+++ Tesis_Algoritmo_Genetico/Program.cs
@@ -61,21 +61,22 @@ namespace Tesis_Algoritmo_Genetico
             }
             /*Contador punto*/
 
-            if (a1>=a2)
+            if (a1 >= a2)
             {
-               
-                matriz = new char[a2+1, a1 + a2];
-                for(int x=0; x<a2; x++)
+
+                matriz = new char[a2 + 2, a1 + a2+1];
+                for (int x = 0; x <= a2+1; x++)
                 {
-                    for(int y=0;y< a1 + a2; y++)
+                    for (int y = 0; y < a1 + a2+1; y++)
                     {
-                        matriz[x,y] = '0';
+                        matriz[x, y] = '0';
                     }
                 }
-                
-                
+
+
                 int renglon = 1;
-                for (int i = array2.Length-1; 0 < i; i--)
+                int escalera = 0;
+                for (int i = array2.Length - 1; 0 <= i; i--)
                 {
                     int ancho = a1 + a2 - 1;
                     acarreo = new char[a1];
@@ -84,30 +85,56 @@ namespace Tesis_Algoritmo_Genetico
                         acarreo[x] = '0';
                     }
                     int numero1 = int.Parse(array2[i].ToString());
-                    for (int j = array1.Length-1; 0 <=j ; j--)
+                    for (int j = array1.Length - 1; 0 <= j; j--)
                     {
                         int numero2 = int.Parse(array1[j].ToString());
-                        int sumacarreo= Int32.Parse(acarreo[j].ToString());
-                        int mult = (numero1 * numero2)+sumacarreo;
+                        int sumacarreo = Int32.Parse(acarreo[j].ToString());
+                        int mult = (numero1 * numero2) + sumacarreo;
                         string con1 = mult.ToString();
                         char[] myStringchar = con1.ToCharArray();
-                        if(con1.Length==2)
+                        if (con1.Length == 2)
                         {
-                            acarreo[j-1] = myStringchar[0];
-                            matriz[renglon,ancho] = myStringchar[1];
+                            if (j == 0)
+                            {
+                                matriz[renglon, ancho - escalera - 1] = myStringchar[0];
+                                matriz[renglon, ancho - escalera] = myStringchar[1];
+                            }
+                            else
+                            {
+                                acarreo[j - 1] = myStringchar[0];
+                                matriz[renglon, ancho - escalera] = myStringchar[1];
+                            }
                         }
                         else
                         {
-                            matriz[renglon, ancho] = myStringchar[0];
+                            matriz[renglon, ancho - escalera] = myStringchar[0];
                         }
                         ancho = ancho - 1;
                     }
-                    renglon = renglon - 1;
+                    escalera = escalera + 1;
+                    renglon = renglon + 1;
+                }
+                int acumuladorsuma = 0;
+                for (int j = a1 + a2 - 1; 0 < j; j--)
+                {
+                    for (int i = 0; i <= a2; i++)
+                    {
+                        int numero1 = int.Parse(matriz[i, j].ToString());
+                        acumuladorsuma = acumuladorsuma + numero1;
+                    }
+                    string suma = acumuladorsuma.ToString();
+                    char[] charsuma = suma.ToCharArray();
+                    if (suma.Length == 2)
+                    {
+                    }
+                    else
+                    {
+                    }
                 }
             }
             else
             {
-                matriz = new char[a2+1, a1 + a2];
+                matriz = new char[a2 + 1, a1 + a2];
             }
 
             
