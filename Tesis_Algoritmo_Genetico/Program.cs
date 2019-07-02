b1b5d250210c31093c11e53ab903008c249b2261
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-07-02 12:48:23 -0500

    Avances de tesis

--- Tesis_Algoritmo_Genetico/Program.cs
+++ Tesis_Algoritmo_Genetico/Program.cs
@@ -23,7 +23,9 @@ namespace Tesis_Algoritmo_Genetico
             Console.WriteLine("Introduzca la valor de salvamento: ");
             vss = Console.ReadLine();
             Console.WriteLine("Introduzca el VPN posible: ");
-            vpn = Console.ReadLine();
+            vpn = Console.ReadLine();           
+
+
 
             decimal[] FNE = new decimal[Convert.ToInt32(per)];
             Random rand = new Random();
@@ -32,7 +34,7 @@ namespace Tesis_Algoritmo_Genetico
             
             for (int x=0; x<Convert.ToInt32(per);x++)
             {
-                Double numeroaleatorio = rand.NextDouble() * 2000000;
+                Double numeroaleatorio = rand.NextDouble() * 950000;
                 numeroaleatorio = Math.Round(numeroaleatorio, 4);
                 // Console.WriteLine("\tIntroduzca el FNE {0}: ", x+1);
                 //  String temporal= Console.ReadLine();
@@ -45,9 +47,11 @@ namespace Tesis_Algoritmo_Genetico
             VPN = Convert.ToDecimal(vpn);
 
             Console.ReadKey();
-            DateTime tiempo1 = DateTime.Now;
+            Console.Write("\n\n\n\n\nCorre tiempo calculando\n\n\n\n\n");
+            DateTime tiempo1 = DateTime.Now;            
             ResultadoVPN = CalcularVPN(inversion, FNE, VS, VPN / 100, periodo);
-            Console.Write("\n\n RESULTADO DE VPN: {0}", ResultadoVPN.ToString("0,0.0000")); 
+            Console.Write("\n\n RESULTADO DE VPN: {0} \n\n", ResultadoVPN.ToString("0,0.0000"));
+            Console.ReadKey();
             if (ResultadoVPN>0)
             {
                 ResultadoTIR=CalcularTIR(VPN/100, 1, inversion, FNE, VS, periodo);
@@ -57,10 +61,10 @@ namespace Tesis_Algoritmo_Genetico
                 ResultadoTIR=CalcularTIR(VPN/100, 2, inversion, FNE, VS, periodo);
             }
             ResultadoTIR = ResultadoTIR * 100;
-            Console.Write("\n\n RESULTADO DE TIR: {0} ", ResultadoTIR.ToString("0,0.0000"));
+            Console.Write("\n\n RESULTADO DE TIR: {0} ", ResultadoTIR.ToString());
             DateTime tiempo2 = DateTime.Now;
             TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
-            Console.Write("\n\n\n\n TIEMPO TOTAL DE EJECUCION: {0} ", total.ToString());            
+            Console.Write("\n\n\n\n TIEMPO TOTAL DE EJECUCION: {0} ", total.ToString());          
             Console.ReadKey();
         }
 
@@ -75,7 +79,9 @@ namespace Tesis_Algoritmo_Genetico
                 case 1:
                     do
                     {
+                        
                         Resultado = CalcularVPN(inversion, FNE, VdS, ValorTIRR, n);//Cabie aqui tenia hasta 10
+                        Console.Write("1.---Ya es igual a 0? = {0} \n", Resultado);
                         if (MenosCero == true)
                         {
                             TasaIncDec = TasaIncDec / 2;
@@ -97,6 +103,7 @@ namespace Tesis_Algoritmo_Genetico
                     do
                     {
                         Resultado = CalcularVPN(inversion, FNE, VdS, ValorTIRR, n);
+                        Console.Write("2.---Ya es igual a 0? = {0} \n", Resultado);
                         if (MenosCero == true)
                         {
                             TasaIncDec = TasaIncDec / 2;
@@ -120,27 +127,34 @@ namespace Tesis_Algoritmo_Genetico
 
         public static decimal CalcularVPN(decimal Inversion, decimal[] FNE, decimal VS, decimal TMAR, int Periodo)
         {
+            //VAN
             decimal FNEAcumulado = 0, fVPN = 0;
             int i = 0;
-            try
-            {
+           /* try
+            {*/
                 decimal DivTMAR = 1M + TMAR;
-                Console.WriteLine("\n\n\n\nCalculo de VPN\n");
                 for (i = 1; i < Periodo; i++)
                 {
-                    decimal valorinferior = (decimal)Math.Pow((double)DivTMAR, i);
-                    FNEAcumulado = FNEAcumulado + FNE[i - 1] / valorinferior;
-                    Console.WriteLine("Pos: {0} Valor: {1}",i,FNEAcumulado);
+                    decimal valorinferior = Decimal.Round((decimal)Math.Pow((double)DivTMAR, i),10);
+                    string str = valorinferior.ToString();
+                    valorinferior= Convert.ToDecimal(str);
+                    FNEAcumulado = Decimal.Round(FNEAcumulado + FNE[i - 1] / valorinferior,10);
+                    string str2 = FNEAcumulado.ToString();
+                    FNEAcumulado = Convert.ToDecimal(str2); 
                 }
-                Console.WriteLine("\n\n\n\n\n");
-                decimal valorinferiorF = (decimal)Math.Pow((double)DivTMAR, i);
-                FNEAcumulado = FNEAcumulado + ((FNE[i - 1] + VS) / valorinferiorF);
-                fVPN = FNEAcumulado - Inversion;
-            }
+                decimal valorinferiorF = Decimal.Round((decimal)Math.Pow((double)DivTMAR, i),10);
+                string str3 = valorinferiorF.ToString();
+                valorinferiorF = Convert.ToDecimal(str3);
+                FNEAcumulado = Decimal.Round(FNEAcumulado + ((FNE[i - 1] + VS) / valorinferiorF),10);
+                string str4 = FNEAcumulado.ToString();
+                FNEAcumulado = Convert.ToDecimal(str4);
+                fVPN = FNEAcumulado - Inversion;                
+
+           /* }
             catch (OverflowException e)
             {
-                Console.WriteLine("Exception: {0} > {1}.", e, decimal.MaxValue);
-            }
+                Console.WriteLine("A sobre pasado el tamaño del decimal Exception: {0} > {1}.", e, decimal.MaxValue);
+            }*/
             return fVPN;
         }
     }
