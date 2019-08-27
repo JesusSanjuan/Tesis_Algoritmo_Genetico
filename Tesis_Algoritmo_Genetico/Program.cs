6d3188fa3c6c6a3a5182af7985038fa011408510
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-08-27 13:35:24 -0500

    Corrreccion

--- Tesis_Algoritmo_Genetico/Program.cs
+++ Tesis_Algoritmo_Genetico/Program.cs
@@ -15,6 +15,40 @@ namespace Tesis_Algoritmo_Genetico
         
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            byte[] bytes = new byte[20];
+            rand.NextBytes(bytes);
+            decimal inicio = 500000;
+            double lowerBound = (double)inicio - 20000;
+            double upperBound = (double)inicio + 20000;
+
+            decimal inversion = (decimal)(rand.NextDouble() * (upperBound - lowerBound) + lowerBound);//////////////////
+
+            Random randNum = new Random();
+            int per = randNum.Next(12, 600);////////////////////////////
+
+            Random rand2 = new Random();
+            byte[] bytes2 = new byte[20];
+            rand2.NextBytes(bytes2);
+            double lowerBound2 = (double)0;
+            double upperBound2 = (double)20000;
+
+            decimal VS = (decimal)(rand2.NextDouble() * (upperBound2 - lowerBound2) + lowerBound2);//////////////////
+
+
+            decimal[] FNE = new decimal[per];
+            for (int num = 0; num < per; num++)
+            {
+                Random rand3 = new Random();
+                byte[] bytes3 = new byte[20];
+                rand3.NextBytes(bytes3);
+                decimal inicio3 = 5000;
+                double lowerBound3 = (double)inicio3 - 2000;
+                double upperBound3 = (double)inicio3 + 2000;
+                FNE[num] = (decimal)(rand3.NextDouble() * (upperBound3 - lowerBound3) + lowerBound3);//////////////////
+            }
+
+            
             StreamWriter outputFile = new StreamWriter("D:\\Archivo.txt");
             string x = "Holai";
             outputFile.WriteLine(x+"\t Hola");
